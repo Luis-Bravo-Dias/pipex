fd755c9d00e958e175daeec962acf2cee9e9e7b1
Luis miguel Leiria bravo rodrigues dias <lleiria-@c2r10s7.42lisboa.com>
2022-07-21 17:21:39 +0100

    6

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 15:11:45 by lleiria-          #+#    #+#             */
-/*   Updated: 2022/07/21 10:37:17 by lleiria-         ###   ########.fr       */
+/*   Updated: 2022/07/21 15:46:54 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,35 @@ void	verify(int fd, int fork, int pipe)
 		perror("");
 }
 
-int	main(int ac, char **av)
+int	main(int ac, char **av, char **env)
 {
 	//int	fd;
 
-	if (ac < 4)
-		ft_printf("Insufficient arguments\n");
+	/*if (ac < 4)
+		ft_printf("Insufficient arguments\n");*/
 	(void)av;
+	(void)ac;
 	/*else
 	{
 		fd = open(av[1], O_RDONLY);
 		//verify (fd);
-	}*/
-	return (0);
+	}
+	return (0);*/
+	int 	i;
+	char	**path;
+
+	i = 0;
+	while (env[i])
+	{
+		if (env[i][0] == 'P' && env[i][1] == 'A')
+			path = ft_split(env[i] + 5, ':');
+		i++;
+	}
+	
+	i = 0;
+	while (path[i])
+	{
+		ft_printf("%s\n", path[i]);
+		i++;
+	}
 }
--- src/pipex.c
+++ src/pipex.c
@@ -6,13 +6,40 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 16:58:58 by lleiria-          #+#    #+#             */
-/*   Updated: 2022/07/21 11:27:37 by lleiria-         ###   ########.fr       */
+/*   Updated: 2022/07/21 16:03:56 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //end[1] -> child process (writes)
 //end[0] -> parent process (reads)
 #include "../pipex.h"
+
+#define PID 1
+
+
+void	errors(int nbr, int id)
+{
+	if (id == 1)
+	{
+		pid 
+	}
+	
+}
+
+void *test(void *test)
+{
+	if (!test)
+	{
+		free(cenas);
+		exit(1);
+	}	
+	else 
+		return (test);
+}
+
+test(strdup())
+
+
 
 void	child_process(int f1, char *cmd1)
 {
